5a2eebdda41f094a513ff37cbbc75960806faa22
Radiquum <kentai.waah@gmail.com>
2023-03-04 00:10:18 +0500

    добавление поиска в виде списка

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-from searchdb import searchDB
+from searchdb import searchDB, searchDBlist, searchMedia
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -28,20 +28,24 @@ async def searchDate(update: Update, context: ContextTypes.DEFAULT_TYPE):
     result = searchDB('timestamp', timestamp)
     message = await context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
-n = 0
-
 async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global n
     n = 0
     
-    global TERM
-    TERM = context.args[1].capitalize()
-    global TYPE
-    TYPE = context.args[0]
+    try:
+        global TERM
+        TERM = context.args[1].capitalize()
+        global TYPE
+        TYPE = context.args[0]
+    except IndexError:
+        await context.bot.sendMessage(chat_id=update.effective_chat.id, text="wrong or not complete command!")
+        return(False)
+    await context.bot.deleteMessage(chat_id=update.effective_chat.id, message_id=update.effective_message.id)
+    
     result = searchDB(TYPE, TERM)
     keyboard = [
         [
-            InlineKeyboardButton("Next", callback_data="+1"),
+            InlineKeyboardButton("Next", callback_data="img +1"),
         ],
         [InlineKeyboardButton(f"{n + 1}/{len(result)}", callback_data="dummy")],
     ]
@@ -50,30 +54,20 @@ async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.sendMessage(chat_id=update.effective_chat.id, text="Nothing found for this date. Retry your search.")
         return(False)
     await context.bot.copyMessage(chat_id=update.effective_chat.id, from_chat_id=chatId, message_id=result[0], reply_markup=reply_markup)
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
-async def navButtons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def imgNavButtons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
-    if query.data == 'dummy':
-        return
     global n
-    n += int(query.data)
+    resp = query.data.strip("img")
+    n += int(resp)
     await query.delete_message()
-    
     result = searchDB(TYPE, TERM)
-    # keyboard = [
-    #     [
-    #         InlineKeyboardButton("Prev", callback_data="-1"),
-    #         InlineKeyboardButton("Next", callback_data="+1"),
-    #     ],
-    #     [InlineKeyboardButton(f"{n}/{len(result)}", callback_data="dummy")],
-    # ]
     if n != len(result):
         keyboard = [
         [
-            InlineKeyboardButton("Prev", callback_data="-1"),
-            InlineKeyboardButton("Next", callback_data="+1"),
+            InlineKeyboardButton("Prev", callback_data="img -1"),
+            InlineKeyboardButton("Next", callback_data="img +1"),
         ],
         [InlineKeyboardButton(f"{n + 1}/{len(result)}", callback_data="dummy")],
     ]
@@ -81,7 +75,7 @@ async def navButtons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
     if n == 0:
         keyboard = [
         [
-            InlineKeyboardButton("Next", callback_data="+1"),
+            InlineKeyboardButton("Next", callback_data="img +1"),
         ],
         [InlineKeyboardButton(f"{n + 1}/{len(result)}", callback_data="dummy")],
     ]
@@ -89,26 +83,114 @@ async def navButtons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
     if n == len(result) - 1:
         keyboard = [
         [
-            InlineKeyboardButton("Prev", callback_data="-1"),
+            InlineKeyboardButton("Prev", callback_data="img -1"),
         ],
         [InlineKeyboardButton(f"{n + 1}/{len(result)}", callback_data="dummy")],
     ]
-        
     reply_markup = InlineKeyboardMarkup(keyboard)
     await context.bot.copyMessage(chat_id=update.effective_chat.id, from_chat_id=chatId, message_id=result[0 + n], reply_markup=reply_markup)
 
+async def searchList(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global n
+    n = 1
+    global offset
+    offset = 0
+    global pages
+    pages = 0
+    try:
+        global TERM
+        TERM = context.args[1].capitalize()
+        global TYPE
+        TYPE = context.args[0]
+    except IndexError:
+        await context.bot.sendMessage(chat_id=update.effective_chat.id, text="wrong or not complete command!")
+        return(False)
+    
+    await context.bot.deleteMessage(chat_id=update.effective_chat.id, message_id=update.effective_message.id)
+    
+    result = searchDBlist(TYPE, TERM, offset)
+    if result == []:
+        await context.bot.sendMessage(chat_id=update.effective_chat.id, text="Nothing found for this date. Retry your search.")
+        return(False)
+    
+    for i in range(len(searchDB(TYPE, TERM))):
+        if i % 6 == 0: 
+            pages += 1
+    
+    keyboard = []
+    for x in result:
+        keyboard.append([InlineKeyboardButton(x, callback_data=f"filename {x}")])
+    if len(result) == 6 and n != pages:
+        keyboard.append([InlineKeyboardButton("Next", callback_data="list +1")])
+    
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.sendMessage(chat_id=update.effective_chat.id, text=f"Available Media. Page {n}/{pages}", reply_markup=reply_markup)
+
+async def listNavButtons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    global n
+    global offset
+    resp = query.data.strip("list")
+    n += int(resp)
+    if int(resp) == -1:
+        offset -= 6
+    else:
+        offset += 6
+    await query.delete_message()
+    result = searchDBlist(TYPE, TERM, offset)
+    
+    if result == []:
+        await context.bot.sendMessage(chat_id=update.effective_chat.id, text="Nothing found for this date. Retry your search.")
+        return(False)
+    keyboard = []
+    for x in result :
+        keyboard.append([InlineKeyboardButton(x, callback_data=f"filename {x}")])
+    if n != pages and n != 0:
+        keyboard.append([InlineKeyboardButton("Prev", callback_data="list -1"),
+            InlineKeyboardButton("Next", callback_data="list +1")])
+    if n == 0 and len(result) == 6:
+        keyboard.append([InlineKeyboardButton("Next", callback_data="list +1")])
+    if n >= pages and len(result) < 6:
+        keyboard.append([InlineKeyboardButton("Prev", callback_data="list -1")])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await context.bot.sendMessage(chat_id=update.effective_chat.id, text=f"Available Media. Page {n}/{pages}", reply_markup=reply_markup)
+
+async def getListMedia(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    resp = query.data.strip("filename ")
+    result = searchMedia('fileName', resp)
+    await context.bot.copyMessage(chat_id=update.effective_chat.id, from_chat_id=chatId, message_id=result[0])
+
+async def dummyButtons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    if query.data == 'dummy':
+        return
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+    
 if __name__ == '__main__':
     
     start_handler = CommandHandler('start', start)
     searchDate_handler = CommandHandler('searchDate', searchDate)
+    searchList_handler = CommandHandler('searchList', searchList)
     search_handler = CommandHandler('search', search)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
     
     application = ApplicationBuilder().token(botToken).build()
     application.add_handler(start_handler)
     application.add_handler(search_handler)
     application.add_handler(searchDate_handler)
+    application.add_handler(searchList_handler)
     application.add_handler(echo_handler)
-    application.add_handler(CallbackQueryHandler(navButtons))
+    application.add_handler(CallbackQueryHandler(imgNavButtons, pattern="^img"))
+    application.add_handler(CallbackQueryHandler(listNavButtons, pattern="^list"))
+    application.add_handler(CallbackQueryHandler(getListMedia, pattern="^filename"))
+    application.add_handler(CallbackQueryHandler(dummyButtons, pattern="^dummy$"))
+    application.add_handler(unknown_handler)
     
     application.run_polling()
--- searchdb.py
+++ searchdb.py
@@ -9,3 +9,19 @@ def searchDB(search_type, value):
         for row in result:
             results.append(row.postId)
     return(results)
+
+def searchDBlist(search_type, value, offset):
+    results = []
+    with engine.connect() as conn:
+        result = conn.execute(text(f"SELECT fileName FROM posts WHERE {search_type} == :{search_type} LIMIT 6 OFFSET {offset}"), {search_type: value})
+        for row in result:
+            results.append(row.fileName)
+    return(results)
+
+def searchMedia(search_type, value):
+    results = []
+    with engine.connect() as conn:
+        result = conn.execute(text(f"SELECT postId, fileName FROM posts WHERE {search_type} == :{search_type}"), {search_type: value})
+        for row in result:
+            results.append(row.postId)
+    return(results)
